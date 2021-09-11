9994b47a7d71609158625f65f4ff6c2f93d1ff78
Rob Jewell <jewell.robertp@gmail.com>
2021-09-11 14:17:15 -0400

    initial

---
+++ src/test/kotlin/com/skromments/comment/CommentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.skromments.comment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
